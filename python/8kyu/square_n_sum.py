8f8f3ec9d435ca34e389495064eb2104964d0c23
BarteG45 <bartosz.graczkowski@wp.pl>
2024-05-07 17:05:20 +0200

    Square(n) Sum done in python

---
+++ python/8kyu/square_n_sum.py
@@ -0,0 +1,10 @@
+#DESCRIPTION
+'''Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9.'''
+
+#SOLUTION
+def square_sum(numbers):
+    sum = 0
+    for num in numbers:
+        sum += num**2
+    return sum
