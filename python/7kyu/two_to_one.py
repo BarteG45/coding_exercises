3cb7e32a2e151ec6f98cb414f231976f018764c2
BarteG45 <bartosz.graczkowski@wp.pl>
2024-05-07 16:34:51 +0200

    Two to One done in python

--- python/7kyu/two_to_one.py
+++ python/7kyu/two_to_one.py
@@ -1,13 +1,13 @@
-#DESCRIPTION:
-#Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+#DESCRIPTION
+'''Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
-#Examples:
-#a = "xyaabbbccccdefww"
-#b = "xxxxyyyyabklmopq"
-#longest(a, b) -> "abcdefklmopqwxy"
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
 
 a = "abcdefghijklmnopqrstuvwxyz"
-#longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"'''
 
 #MY SOLUTION
 def longest(a1, a2):
