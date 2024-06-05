8a09ef337e31f31863056df51111c80c557d7bd2
BarteG45 <bartosz.graczkowski@wp.pl>
2024-06-05 16:17:05 +0200

    zip and list comprehension done in python

---
+++ python/coding_class_exercises/zip_and_listcomprehension.py
@@ -0,0 +1,17 @@
+#DESCRIPTION
+'''
+Create two lists: names = ["Alice", "Bob", "Charlie"] and scores = [85, 92, 78].
+
+Use the zip function to combine these lists into a list of tuples.
+
+Use list comprehension to create a new list containing strings in the format "Name: score" for each element.
+'''
+
+#MY SOLUTION
+names = ["Alice", "Bob", "Charlie"]
+scores = [85, 92, 78]
+
+combined_list = list(zip(names,scores))
+
+formatted_list = [f"{name}: {score}" for name, score in combined_list]
+print(formatted_list)
